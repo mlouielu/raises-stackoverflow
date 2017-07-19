6a54c1d9b4c6a4ca200fb4c8d01c5205aa86bd36
Louie Lu <git@louie.lu>
2017-07-19 17:25:23 +0800

    Add new API and update README

--- README.md
+++ README.md
@@ -2,3 +2,57 @@ Raises Stack Overflow
 --------------------
 
 *When Python raises, We Stack Overflow*
+
+
+Install
+-------
+
+Please use `pip` to install `raises`
+
+```
+$ pip install raises
+```
+
+
+How to use?
+-----------
+
+Is easy to use `raises` in your daily work, to help you find out raises error's
+answer on Stack Overflow, simply `import raises` to your code, then done!
+
+```python
+import raises  # To the top of your source code
+```
+
+Example
+-------
+
+For example, if we type in `int("")` to REPL, it will raise a `ValueError`:
+
+```python
+>>> import raises
+>>> int("")
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+ValueError: invalid literal for int() with base 10: ''
+```
+
+You should now saw a new tab in your browser, to find out the answer on
+Stack Overflow!
+
+
+API
+---
+
+* raises.enable()
+
+To enable the raises function
+
+* raises.disable()
+
+To disable the raises function
+
+* raises.raiser
+
+The core function of raises, it was the `sys.excepthook` replacer to handle
+every exceptio, and to open a new browser with tabs.
--- raises/__init__.py
+++ raises/__init__.py
@@ -2,7 +2,7 @@
 """When Python raises, we Stack Overflow
 """
 
-__version__ = '0.1'
+__version__ = '0.2'
 
 
 import sys
@@ -16,16 +16,28 @@ except ImportError:
 
 
 STACKOVERFLOW_URL = 'https://stackoverflow.com/search?tab=votes&{qs}'
+__enabled = True
 
 
 def raiser(etype, value, tb):
-    error_msg = '{etype}: {value}'.format(etype=etype.__name__, value=value)
-    webbrowser.open(STACKOVERFLOW_URL.format(
-        qs=urlencode({'q': '[python] ' + error_msg})), new=2)  # new=2 open in new tab
+    if __enabled:
+        error_msg = '{etype}: {value}'.format(etype=etype.__name__, value=value)
+        webbrowser.open(STACKOVERFLOW_URL.format(
+            qs=urlencode({'q': '[python] ' + error_msg})), new=2)  # new=2 open in new tab
 
     # sys.__excepthook__ is the original excepthook, perform to print traceback
     # See https://docs.python.org/3/library/sys.html#sys.__excepthook__
     sys.__excepthook__(etype, value, tb)
+
+
+def disable():
+    global __enabled
+    __enabled = False
+
+
+def enable():
+    global __enabled
+    __enabled = True
 
 
 # Globaly replace sys.excepthook to raiser
